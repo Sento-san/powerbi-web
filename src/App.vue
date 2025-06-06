ceae8277163af510ebde643310eec4be4726c461
Josh Jois <pulga10josh@gmail.com>
2025-06-06 16:39:58 -0600

    Logro:Primer commit powerBI web

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+        <h1>Reporte de Power BI</h1>
+        <p>Visualización interactiva de
+            datos generada en Power BI.</p>
+        <div class="iframe-container">
+            <h1>HOLA PUTA</h1>
+        </div>
+    </div>
+</template>
+
