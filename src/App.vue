159162534119e1c83b1cf6a4d8543496428c6e59
Josh Jois <pulga10josh@gmail.com>
2025-06-06 17:21:06 -0600

    Logro: iframe de power BI agregado

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
         <p>Visualización interactiva de
             datos generada en Power BI.</p>
         <div class="iframe-container">
+            <iframe title="NombreProvicional" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiOGVmMDg2MmQtM2MyZC00MWY5LThkNzUtNTE2ZWIwZTQyMTE4IiwidCI6ImFkNGZhYmFkLWVmMTgtNDZjNy1hMDg2LTZkM2MxOGY1MDExZiJ9" frameborder="0" allowFullScreen="true"></iframe>
             <h1>HOLA PUTA</h1>
         </div>
     </div>
